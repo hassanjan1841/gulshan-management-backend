a74fc5002bdcdd1557edd61b94f1b92b87d63b69
Hassan Jan <161922826+hassanjan1841@users.noreply.github.com>
2024-12-21 13:03:21 +0500

    the error fetching is now more accurate and fixed , the taost is working fine for the errors

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,13 +3,14 @@ import { generateAccessToken } from "../helpers/generateToken.js";
 // Get a specific user by ID
 const login = async (req, res) => {
   try {
-    console.log(req.body);
-    const { email } = req.body;
+    // console.log(req.body);
     // Find user by ID
-    const data = await User.find({ email: email });
+    const data = await User.find({ email: req.body.email });
     const user = data[0];
-    const token = generateAccessToken(user.toJSON());
+    const token = generateAccessToken(user);
+
     console.log("token", token);
+
     if (!token) {
       return res.status(401).json({ message: "Invalid credentials." });
     }
